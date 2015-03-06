3a92ed23dbd828c44e49a4a279ddb7ede7488ce4
Marc Bradshaw <marc@marcbradshaw.net>
2015-03-06 11:33:12 +1100

    build for fastmail Fri Mar  6 11:33:12 AEDT 2015
    commit a4bb4a025a70078262c0595942f64154188629a3
    Merge: 5d2e6b4 cc2a306
    Author: Marc Bradshaw <marc@marcbradshaw.net>
    Date:   Fri Mar 6 11:32:24 2015 +1100
    
        Merge branch 'upstream'
    
    commit cc2a306e903aad3660e2192a30f0dc7f3b019bfe
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Tue Mar 3 22:41:44 2015 -0800
    
        lower case domain names to at entry points
    
    commit 8a8b449af44d621246351b549fc6bf640adc6e94
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Tue Mar 3 21:01:17 2015 -0800
    
        handle substitution of = with : in DMARC DNS rec
    
    commit 1dc5b56cc38fda6fb8f761bbfbdfa114da686681
    Merge: 53563c9 177c3ae
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Tue Mar 3 20:36:59 2015 -0800
    
        Merge pull request #54 from msimerson/revert-52-file_sharedir_install
    
        Revert "Missing PreReq added"
    
    commit 177c3ae96ccaab978aedfed5ac25b092bf980311
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Tue Mar 3 07:43:31 2015 -0800
    
        Revert "Missing PreReq added"
    
    commit 5d2e6b4413e366d1973ef9ab77d3a3174f53c1da
    Merge: f929484 53563c9
    Author: Marc Bradshaw <marc@marcbradshaw.net>
    Date:   Tue Mar 3 13:36:19 2015 +1100
    
        Merge branch 'upstream'
    
    commit 53563c9e8bf84c7632f0608271f20509ac35531b
    Merge: a1d408a 0663861
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Mon Mar 2 11:57:02 2015 -0800
    
        Merge pull request #52 from fastmail/file_sharedir_install
    
        Missing PreReq added
    
    commit a1d408a657aeafc04dd217d5f67a5a6f57f85f88
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Fri Feb 27 21:02:17 2015 -0800
    
        v1.20150228
    
         - fix the policy_evaluated fields in outbound reports
         - accommodate a common DMARC error substiting = with :
         - initialized config file first (was non-deterministic)
         - tolerate missing SPF auth type scope
    
    commit 853689a332b5a8c902bdace1ad9af04080f701f5
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Fri Feb 27 21:00:47 2015 -0800
    
        update Changes for release
    
    commit c075e85b8e6a07ac7b79880cc07877fb7f1eb3bb
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Fri Feb 27 20:56:49 2015 -0800
    
        policy: accommodate common substituion of = with :
    
        in DNS published DMARC policies.
    
        This properly parses the DMARC authors intents for the majority of DMARC parse failures witnessed in Issue #39
    
    commit c351fadf60183cb869edd73aed9f41ef172d1e50
    Merge: 38ad62d 819340b
    Author: Matt Simerson <matt@tnpi.net>
    Date:   Fri Feb 27 20:54:23 2015 -0800
    
        Merge pull request #50 from fastmail/fix_report_policy_evaluated
    
        Fix the policy_evaluated fields in outbound reports

--- lib/Mail/DMARC.pm
+++ lib/Mail/DMARC.pm
@@ -1,5 +1,5 @@
 package Mail::DMARC;
-our $VERSION = '1.20150301'; # VERSION
+our $VERSION = '1.20150306'; # VERSION
 use strict;
 use warnings;
 
@@ -249,7 +249,7 @@ Mail::DMARC - Perl implementation of DMARC
 
 =head1 VERSION
 
-version 1.20150301
+version 1.20150306
 
 =head1 SYNOPSIS
 
