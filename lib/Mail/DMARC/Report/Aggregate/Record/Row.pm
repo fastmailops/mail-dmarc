02a06ab865bcec9a2542658ca6299a8576d096c4
Marc Bradshaw <marc@marcbradshaw.net>
2015-03-11 16:52:57 +1100

    build for fastmail Wed Mar 11 16:52:57 AEDT 2015

--- lib/Mail/DMARC/Report/Aggregate/Record/Row.pm
+++ lib/Mail/DMARC/Report/Aggregate/Record/Row.pm
@@ -1,5 +1,5 @@
 package Mail::DMARC::Report::Aggregate::Record::Row;
-our $VERSION = '1.20150306'; # VERSION
+our $VERSION = '1.20150311'; # VERSION
 use strict;
 use warnings;
 
@@ -58,7 +58,7 @@ Mail::DMARC::Report::Aggregate::Record::Row - row section of a DMARC aggregate r
 
 =head1 VERSION
 
-version 1.20150306
+version 1.20150311
 
 =head1 AUTHORS
 
